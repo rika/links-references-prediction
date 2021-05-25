3f2ed2484f867cf90eaabb18fa2ae680ee950dff
Ricardo Oda <ricardo.oda@escale.com.br>
2021-05-25 20:32:04 -0300

    refactor: remove parse responsability from scraper

--- src/scraper/scraper.py
+++ src/scraper/scraper.py
@@ -1,50 +1,22 @@
-import re
 import requests
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
-from bs4 import BeautifulSoup as BS
 
-Pattern = type(re.compile('', 0))
+
+class NoStatusCode200(Exception):
+    pass
 
 
 class Scraper:
-    def __init__(self, url):
-        self.url = url.rstrip('/')
-
-    def get_links(self) -> list:
-        response = self.__retry_get_url(self.url)
+    def execute(self, url, parser=None, default=None) -> list:
+        response = self.__retry_get_url(url)
 
         # consider only 200 (no redirects)
         if response.status_code == 200:
             html = str(response.text)
-            return self.__parse_links(html)
+            return html
         else:
-            return []
-
-    def __parse_links(self, html) -> list:
-        soup = BS(html, "html.parser")
-        links = [e.get('href') for e in soup.find_all('a')]
-
-        # remove empty links
-        links = [link for link in links if self.__valid_url(link)]
-
-        # ignore querystring
-        links = [re.search('[^?]*', link).group(0) for link in links]
-
-        # remove duplicates
-        links = list(set(links))
-
-        # handle internal links
-        def map_func(link):
-            link = link.rstrip('/')
-            return self.url + link if (link.startswith('/')) else link
-
-        links = list(map(map_func, links))
-
-        return links
-
-    def __valid_url(self, url):
-        return url is not None and url.startswith(('http', '/'))
+            raise NoStatusCode200()
 
     def __retry_get_url(self, url):
         retry_strategy = Retry(
@@ -56,4 +28,4 @@ class Scraper:
         session.mount("https://", adapter)
         session.mount("http://", adapter)
 
-        return session.get(url)
+        return session.get(url, allow_redirects=False)
